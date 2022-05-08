97e6e3e3ee79fb48190d7b057f602d990e5e94eb
sapphonie <sappho@sappho.io>
2022-05-08 06:28:04 +0000

    add soap_tournament

--- getsoap.sh
+++ getsoap.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 /home/steph/tfTEST/tf2/tf/addons/sourcemod/scripting/spcomp -i ./addons/sourcemod/scripting/include/ addons/sourcemod/scripting/soap_tf2dm.sp -o addons/sourcemod/plugins/soap_tf2dm.smx
+sync; sleep 1
 /home/steph/tfTEST/tf2/tf/addons_1.10/sourcemod/scripting/spcomp -i ./addons/sourcemod/scripting/include/ addons/sourcemod/scripting/soap_tf2dm.sp -o addons/sourcemod/plugins/soap_tf2dm.smx
+
+/home/steph/tfTEST/tf2/tf/addons/sourcemod/scripting/spcomp -i ./addons/sourcemod/scripting/include/ addons/sourcemod/scripting/soap_tournament.sp -o addons/sourcemod/plugins/soap_tournament.smx
+sync; sleep 1
+/home/steph/tfTEST/tf2/tf/addons_1.10/sourcemod/scripting/spcomp -i ./addons/sourcemod/scripting/include/ addons/sourcemod/scripting/soap_tournament.sp -o addons/sourcemod/plugins/soap_tournament.smx
