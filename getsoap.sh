6a78b0e60110d6a0ad2b94428d6b7c3131faefe5
sapphonie <sappho@sappho.io>
2022-08-16 03:33:15 +0000

    update file, recompile

--- getsoap.sh
+++ getsoap.sh
@@ -6,3 +6,5 @@ sync; sleep 1
 /home/steph/tfTEST/tf2/tf/addons/sourcemod/scripting/spcomp -i ./addons/sourcemod/scripting/include/ addons/sourcemod/scripting/soap_tournament.sp -o addons/sourcemod/plugins/soap_tournament.smx
 sync; sleep 1
 /home/steph/tfTEST/tf2/tf/addons_1.10/sourcemod/scripting/spcomp -i ./addons/sourcemod/scripting/include/ addons/sourcemod/scripting/soap_tournament.sp -o addons/sourcemod/plugins/soap_tournament.smx
+
+cp ./addons/sourcemod/plugins/* /home/steph/tfTEST/tf2/tf/addons/sourcemod/plugins/ -rfv
